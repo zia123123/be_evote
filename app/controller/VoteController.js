1b56fa66711e4b7ea34f9c0877aa86fca5c69683
zia123123 <ziamuhamad@gmail.com>
2021-08-28 20:38:57 +0700

    fix bugs

--- app/controller/VoteController.js
+++ app/controller/VoteController.js
@@ -35,8 +35,10 @@ module.exports = {
         let vote = await votes.findAll({
             where: {
                 archived: false,
-                rt: req.params.rt, 
                 [Op.or]: [
+                    { rt: req.params.rt, }
+                  ],
+                  [Op.or]: [
                     { rw: true }
                   ]
             },
